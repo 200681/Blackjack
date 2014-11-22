60eb8ba2cd9d463527dd6bc08ee82be7a26f52b6
vduuude <vduuude@gmail.com>
2014-11-22 14:22:28 +0800

    Added more stuff

--- src/com/xtremedesign/blackjack/Screen.java
+++ src/com/xtremedesign/blackjack/Screen.java
@@ -3,7 +3,9 @@ package com.xtremedesign.blackjack;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -12,7 +14,9 @@ public class Screen extends JPanel implements Runnable {
 	Thread th = new Thread(this);
 	static List<Card> cards;
 	boolean addcard = false;
+	String cardtype;
 	public List<String> possiblecards = new ArrayList<String>();
+	public HashMap<String, Integer> values = new HashMap<String, Integer>();
 	int x = 100;
 	int y = 100;
 	int value = 0;
@@ -22,6 +26,57 @@ public class Screen extends JPanel implements Runnable {
 		th.start();
 	}
 	
+	
+	public void registerHashmap() {
+		values.put("c2", 2);
+		values.put("c3", 3);
+		values.put("c4", 4);
+		values.put("c5", 5);
+		values.put("c6", 6);
+		values.put("c7", 7);
+		values.put("c8", 8);
+		values.put("c9", 9);
+		values.put("c10", 10);
+		values.put("cj", 10);
+		values.put("cq", 10);
+		values.put("ck", 10);
+		values.put("h2", 2);
+		values.put("h3", 3);
+		values.put("h4", 4);
+		values.put("h5", 5);
+		values.put("h6", 6);
+		values.put("h7", 7);
+		values.put("h8", 8);
+		values.put("h9", 9);
+		values.put("h10", 10);
+		values.put("hj", 10);
+		values.put("hq", 10);
+		values.put("hk", 10);
+		values.put("d2", 2);
+		values.put("d3", 3);
+		values.put("d4", 4);
+		values.put("d5", 5);
+		values.put("d6", 6);
+		values.put("d7", 7);
+		values.put("d8", 8);
+		values.put("d9", 9);
+		values.put("d10", 10);
+		values.put("dj", 10);
+		values.put("dq", 10);
+		values.put("dk", 10);
+		values.put("s2", 2);
+		values.put("s3", 3);
+		values.put("s4", 4);
+		values.put("s5", 5);
+		values.put("s6", 6);
+		values.put("s7", 7);
+		values.put("s8", 8);
+		values.put("s9", 9);
+		values.put("s10", 10);
+		values.put("sj", 10);
+		values.put("sq", 10);
+		values.put("sk", 10);
+	}
 	public void updateCards() {
 		possiblecards.clear();
 		possiblecards.add("c1");
@@ -79,8 +134,13 @@ public class Screen extends JPanel implements Runnable {
 	}
 	
 	public synchronized void addCard() {
-		Card card = new Card(x, y)
-		
+		Random random = new Random();
+		String s = possiblecards.get(random.nextInt(possiblecards.size()));
+		cardtype = s;
+		int value = values.get(cardtype);
+		Card card = new Card(cardtype, x, y, value, true);
+		cards.add(card);
+		cardtype = null;
 	}
 	
 	protected void paintComponent(Graphics g) {
